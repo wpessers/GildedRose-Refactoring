e5f81dbbe82e118fca12af729339934c7111124b
wpessers <warre.pessers@gmail.com>
2024-02-19 09:12:57 +0100

    Create abstract item class containing all relevant methods for updating items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,62 +9,8 @@ class GildedRose {
 
     public void updateItems() {
         for (Item item : items) {
-            updateItem(item);
-        }
-    }
-
-    private static void updateItem(Item item) {
-        updateQuality(item);
-        updateSellIn(item);
-        updateExpiredQuality(item);
-    }
-
-    private static void updateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            incrementQuality(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            incrementQuality(item);
-
-            if (item.sellIn < 11) {
-                incrementQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                incrementQuality(item);
-            }
-        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            decrementQuality(item);
-        }
-    }
-
-    private static void updateSellIn(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        item.sellIn--;
-    }
-
-    private static void updateExpiredQuality(Item item) {
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-                incrementQuality(item);
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = 0;
-            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                decrementQuality(item);
-            }
-        }
-    }
-
-    private static void decrementQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality--;
-        }
-    }
-
-    private static void incrementQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
+            AbstractItem abstractItem = new AbstractItem(item);
+            abstractItem.updateItem();
         }
     }
 }
